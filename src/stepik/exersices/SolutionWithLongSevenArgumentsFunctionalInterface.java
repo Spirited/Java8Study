4ed17e5ae52ec58a205c2c9cf1bae627b91f9a66
Sergey Kompanets <sergey_kompanets@ukr.net>
2017-09-11 16:45:39 +0300

    continue study

--- src/stepik/exersices/SolutionWithLongSevenArgumentsFunctionalInterface.java
+++ src/stepik/exersices/SolutionWithLongSevenArgumentsFunctionalInterface.java
@@ -2,8 +2,8 @@ package stepik.exersices;
 
 public class SolutionWithLongSevenArgumentsFunctionalInterface {
     public static void main(String[] args) {
-        SevenArgumentsFunctionalInterface<String,String,String,String,String,String,String,String> solution = (x,y,z,k,m,n,v) -> {
-            return (x+y+z+k+m+n+v).toUpperCase();
-        };
+        SevenArgumentsFunctionalInterface<String,String,String,String,String,String,String,String> solution = (x,y,z,k,m,n,v) -> (x+y+z+k+m+n+v).toUpperCase();
+
+        System.out.println();
     }
 }
