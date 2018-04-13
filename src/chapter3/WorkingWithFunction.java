612fb03b8b506b322b9cdc98e7e6a82fbcdf3a66
Sergey Kompanets <sergey.kompanets@nextiva.com>
2018-04-13 16:16:28 +0300

    added Hive

--- src/chapter3/WorkingWithFunction.java
+++ src/chapter3/WorkingWithFunction.java
@@ -20,7 +20,7 @@ public class WorkingWithFunction {
     }
 
     public static void main(String[] args) {
-        List<Integer> list = map(Arrays.asList("Java", "C++", "Cobol", "Go", "Spark"), (String s) -> s.length());
+        List<Integer> list = map(Arrays.asList("Java", "C++", "Cobol", "Go", "Spark", "Hive"), (String s) -> s.length());
 
         System.out.println(list);
     }
